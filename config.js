e406494e65fa50afc4e14f5dd444fe425a5a2ebc
HoangTien12345 <95924471+HoangTien12345@users.noreply.github.com>
2021-12-10 23:11:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb : "Demo" , 
+    introTitle : 'Gửi em' ,
+    introDesc : Thường  lệ  vốn  trái đất
+    Mà   sao   em   cứ   nhầm   đường
+    Lạc    vào    tim    anh    lẻ    loi
+    Đằng   sau   chữ   yêu   đây   là   thương  ',
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    title: 'Phải chăng em yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
@@ -16,5 +16,4 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink : 'https://github.com/zukahai/Confess-Crush'  // liên kết lộn xộn của các bạn. VD: https://m.me/nam.nodem
